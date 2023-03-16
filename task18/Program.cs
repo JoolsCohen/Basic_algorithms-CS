3b6325f6bf14a47c96e8c4cf991827f8c6d7c0ec
«Julie» <joolescohen@gmail.com>
2023-03-16 11:24:14 +0300

    task18 done

--- task18/Program.cs
+++ task18/Program.cs
@@ -3,16 +3,16 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти");
-string quarterNum = Console.ReadLine();
+int quarterNum = Convert.ToInt32(Console.ReadLine());
 
-string result = RangeCoord(quarterNum);
-Console.WriteLine(result);
+string quarter = RangeCoord(quarterNum);
+Console.WriteLine(quarter);
 
-string RangeCoord (string quarter)
+string RangeCoord (int quarter)
 {
-    if (quarter == "1") return "x > 0, y > 0";
-    if (quarter == "2") return "x < 0, y > 0";
-    if (quarter == "3") return "x < 0, y < 0";
-    if (quarter == "4") return "x > 0, y < 0";
-    return "Введена некорректно четверть";
+if (quarter == 1) return "x > 0, y > 0";
+if (quarter == 2) return "x < 0, y > 0";
+if (quarter == 3) return "x < 0, y < 0";
+if (quarter == 4) return "x > 0, y < 0";
+return "Введена некорректно четверть";
 }
