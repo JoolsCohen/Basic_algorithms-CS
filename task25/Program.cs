27280f1c77b0c4d27589fddfdccdc0150280c8ee
«Julie» <joolescohen@gmail.com>
2023-03-20 00:15:13 +0300

    task25 done

--- task25/Program.cs
+++ task25/Program.cs
@@ -7,18 +7,35 @@ double A = Convert.ToInt64(Console.ReadLine());
 Console.WriteLine("Введите натуральное число B: ");
 double B = Convert.ToInt64(Console.ReadLine());
 
-Degree(A, B);
+// Degree(A, B);
+
+// void Degree(double a, double b)
+// {
+//    if (b >= 0)
+//    {
+//     int resPos = (int)Math.Pow(a, b);
+//     Console.WriteLine(A + " в степени " + B + " равно: " + resPos);
+//    }
+//   if (b < 0)
+//   {
+//     double resNeg = 1.0 / Math.Pow(a, -b);
+//     Console.WriteLine(A + " в степени " + B + " равно: " + resNeg);
+//   }
+// }
+//-----------------------------Второе решение------------------------------
 
 void Degree(double a, double b)
 {
-   if (b >= 0)
-   {
-    int resPos = (int)Math.Pow(a, b);
-    Console.WriteLine(A + " в степени " + B + " равно: " + resPos);
-   }
-  if (b < 0)
+   double resPos = a;
+  for (double i = 1; i < b; i++)
   {
-    double resNeg = 1.0 / Math.Pow(a, -b);
+    resPos *= a;
+  }
+      Console.WriteLine(A + " в степени " + B + " равно: " + resPos);
+}
+  if (B < 0)
+  {
+    double resNeg = 1.0 / Math.Pow(A, -B);
     Console.WriteLine(A + " в степени " + B + " равно: " + resNeg);
   }
-}
+  else Degree(A, B);
