4745331c27b89587850aa71d52880f006d9b9e8c
«Julie» <joolescohen@gmail.com>
2023-03-19 12:12:58 +0300

    task25 done

--- task25/Program.cs
+++ task25/Program.cs
@@ -6,7 +6,6 @@ Console.WriteLine("Введите натуральное число A: ");
 double A = Convert.ToInt64(Console.ReadLine());
 Console.WriteLine("Введите натуральное число B: ");
 double B = Convert.ToInt64(Console.ReadLine());
-double num = A;
 
 Degree(A, B);
 
@@ -14,12 +13,12 @@ void Degree(double a, double b)
 {
    if (b >= 0)
    {
-    int num = (int)Math.Pow(a, b);
-    Console.WriteLine(A + " в степени " + B + " равно: " + num);
+    int resPos = (int)Math.Pow(a, b);
+    Console.WriteLine(A + " в степени " + B + " равно: " + resPos);
    }
   if (b < 0)
   {
-    double y = 1.0 / Math.Pow(a, -b);
-    Console.WriteLine(A + " в степени " + B + " равно: " + y);
+    double resNeg = 1.0 / Math.Pow(a, -b);
+    Console.WriteLine(A + " в степени " + B + " равно: " + resNeg);
   }
 }
