2ef26e4748786b17fc7f3303958897a288ccc5b2
«Julie» <joolescohen@gmail.com>
2023-03-22 19:21:03 +0300

    task09 done

--- task09/Program.cs
+++ task09/Program.cs
@@ -1,30 +1,30 @@
-﻿// 9. Напишите программу, которая выводит
+﻿//  Напишите программу, которая выводит
 // случайное число из отрезка [10, 99] и показывает
 // наибольшую цифру числа.
 // 78 -> 8
 // 12-> 2
 // 85 -> 8
-int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
-// int firstDigit = number / 10;
-// int secondDigit = number % 10;
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {num}");
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
 
-// if (firstDigit > secondDigit) // 88
-//     Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-// else
-//     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
-// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+// переменная  условие                       верное      не верное
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; //тернарное выражение 
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
 
-int result = MaxDigit(number);
-Console.WriteLine($"Наибольшая цифра числа -> {result}");
+// Решение через метод 
 
-int MaxDigit(int num) // num = number
+int MaxDigit(int number)
 {
-    int firstDigit = num / 10;
-    int secondDigit = num % 10;
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
     int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-    
     return maxDigit;
 }
+
+int result = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра числа -> {result}");
