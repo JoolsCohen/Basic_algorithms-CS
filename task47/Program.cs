c5420e92ac3fcae3e98f1209d65cffed4e69cc2f
«Julie» <joolescohen@gmail.com>
2023-03-31 19:37:32 +0300

    done

---
+++ task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Clear();
+Console.WriteLine("Вводим кол-во строчек:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим кол-во столбиков:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+  double[,] matrix = new double[rows, columns];
+  Random rnd = new Random();
+  for (var i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (var j = 0; j < matrix.GetLength(1); j++)
+    {
+      double num = rnd.NextDouble() * (max - min) + min;
+      matrix[i, j] = Math.Round(num, 1);
+      //matrix[i, j] = Math.Round(rnd.NextDouble() * rnd.Next(min, max), 1); // альтернатива
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+  for (var i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (var j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j], 5} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+double[,] array2d = CreateMatrixRndDouble(m, n, 1, 10);
+PrintMatrix(array2d);
---
+++ task50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.Clear();
+Console.WriteLine("Вводим количествово строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вводим количествово столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Вводим номер строки, начиная c 0:  ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Вводим номер столбца, начиная c 0:  ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+  int[,] matrix = new int[rows, columns];
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],3} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+bool FindNumByIndexes(int rows, int columns, int[,] array)
+{
+  if (rows < array.GetLength(0) && columns < array.GetLength(1))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int[,] array2d = CreateMatrixRndInt(m, n, 0, 10);
+PrintMatrix(array2d);
+bool result = FindNumByIndexes(row, column, array2d);
+Console.Write(result ? "Элемент массива с указанными индексоми равен " + array2d[row, column] : "Элемента массива с указанными индексоми не существует");
