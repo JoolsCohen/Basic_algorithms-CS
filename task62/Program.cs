6de167f086e2ea6ce27495fe0ea034f6dc663743
«Julie» <joolescohen@gmail.com>
2023-04-02 14:38:05 +0300

    done

--- task62/Program.cs
+++ task62/Program.cs
@@ -5,13 +5,50 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int n = 4;
+int[,] spiralMatrix = new int[n, n];
 
-//какая то фигня, все заполняется 00
+int temp = 1;
+int i = 0;
+int j = 0;
 
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+  spiralMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(spiralMatrix);
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] / 10 <= 0)
+        Console.Write($" {array[i, j]} ");
+
+      else Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+//------------------------------------------2 решение--------------------
 int n = 4;
 int[,] array = new int[n, n];
 
-int beginI = 0; // При заполнении массива будем очерчивать прямоугольники, каждый из которых на единицу меньше с каждой стороны.
+int beginI = 0; // При заполнении массива будем очерчивать прямоугольники, каждый их которых на единицу меньше с каждой стороны.
 int finalI = 0; // Нам нужно знать точки начала и конца очерчиваемого прямоугольника. Это и будут точки поворота) 
 int beginJ = 0; // Эти 4 точки - это точки поворота. 
 int finalJ = 0; // Они равны длине стороны первого прямоугольника минус длина текущего прямоугольника.
@@ -54,6 +91,7 @@ for (int k = 1; k <= n * n; k++) // Значением k заполняем ма
     }
   }
 }
+
 
 void PrintArray(int[,] array)
 {
