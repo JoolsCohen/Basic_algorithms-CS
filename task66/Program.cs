f21989a6bbc575fb3856b28bf0ea8707ea97bfc2
«Julie» <joolescohen@gmail.com>
2023-04-05 14:34:51 +0300

    done

---
+++ task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.WriteLine("Задайте два натуральных числа:");
+Console.WriteLine("Вводим число А:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим число B:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num1, int num2)
+{
+  if (num1 <= num2) return num1 + SumDigits(num1 + 1, num2);
+  else return 0;
+}
+if (m == n)
+{
+    Console.WriteLine("Числа равны ");
+    return;
+}
+bool NegNumChecking(int num1, int num2)
+{
+  return (num1 < 0 || num2 < 0);
+}
+if (NegNumChecking(m, n))
+{
+  Console.WriteLine("Отрицательные числа не являются натуральными!");
+  return;
+}
+int sumNum = 0;
+if (m <= n) sumNum = SumDigits(m, n);
+if (m > n) sumNum = SumDigits(n, m);
+
+Console.WriteLine($"Cумма натуральных элементов в промежутке от заданных чисел равна: {sumNum}");
---
+++ task68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.WriteLine("Задайте два неотрицательных числа:");
+Console.WriteLine("Первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAckermann(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return FunctionAckermann(m - 1, 1);
+  else return FunctionAckermann(m - 1, FunctionAckermann(m, n - 1));
+}
+bool NegNumChecking(int num1, int num2)
+{
+  return (num1 < 0 || num2 < 0);
+}
+if (NegNumChecking(m, n))
+{
+  Console.WriteLine("Вы ввели отрицательные числа!");
+  return;
+}
+int functionAckermann = FunctionAckermann(m, n);
+Console.Write($"Результат вычисления функции Аккермана чисел {m} и {n} = {functionAckermann} ");
+
+// Функция Аккермана:
+// A(0,n) = n+1
+// A(m,0) = A(m-1,1)
+// A(m,n) = A(m-1,A(m,n-1))
